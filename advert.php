19f647a64e1db3058ec21679b1fbb1882898b363
Jānis <j.locmans@gmail.com>
2015-07-02 15:39:53 +0300

    Project added

---
+++ advert.php
@@ -0,0 +1,45 @@
+<DOCTYPE HTML>
+
+	<html>
+
+		<head>
+			 <link href="style.css" rel="stylesheet" type="text/css">
+			 <script type="text/javascript" src="nav.js"></script>
+		</head>
+	
+		<body>
+
+				<div class="content">
+
+					<header class="main_header">
+
+						<nav class = "main_nav">
+
+							<ul>
+								<li>TEXT</li>
+								<li>TEXT</li>
+								<li>TEXT</li>
+								<li>TEXT</li>
+							<ul>
+
+						</nav>
+
+					</header>
+
+					<aside class = "sidebar">
+
+					</aside>
+
+					<div class = "article">
+
+					</div>
+					
+					<footer class = "main_footer">
+
+					</footer>
+
+				</div>
+
+		</body>
+
+</html>
---
+++ nav.js
@@ -0,0 +1,52 @@
+var get_screen_size = screen.height;
+
+
+window.onload = function () {
+    change_class_style('home', 1, 2, 'height', get_screen_size);
+    change_class_style('home', 1, 2, 'background', 'red');
+}
+
+
+
+function change_class_style(_class, _first, _last, _attr, _value) {
+	
+    var cols  = document.getElementsByClassName(_class);
+    var first = _first - 1;
+    var last;
+
+    	if( _last == 'length' )
+
+    		{ 
+    			last = cols.length; 
+    		} 
+    		else 
+    		{
+    			last = _last - 1;
+    		}
+
+    for(i = 1; first < last; first++) {
+     cols[first].style[_attr] = _value;
+
+    }
+} 
+
+function push(page_id) {
+
+	var current_page = page_id.parentNode.parentNode.getAttribute('id');	
+	var doc =  document.getElementById(current_page);
+	var height = doc.offsetHeight;
+	var	check_size = 0;
+
+	doc.style.height = height;
+
+	var d = setInterval(function () { 
+
+	    height = height - 5;	
+	    doc.style.height = height;
+	    if (height < (get_screen_size * 0.047) && check_size != 1)
+	    	{
+	    		//console.log(document.getElementById(page_id).id);
+	    		check_size = 1;
+	   		}
+	  	}, 10);
+}
